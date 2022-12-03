1e05b9eb8d19e1a4df58347f5518a62eef03df0d
fatih <muhammadfatihmaulana434@gmail.com>
2022-12-03 19:23:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/booklibrary/detail/BooksDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booklibrary.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booklibrary.R
+
+class BooksDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_detail)
+    }
+}
