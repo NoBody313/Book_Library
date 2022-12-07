045d78030502ddf8732fe31873dd2367c3c90661
fatih <muhammadfatihmaulana434@gmail.com>
2022-12-07 23:04:52 +0700

    Login firebase & Google Clear

--- app/src/main/java/com/example/booklibrary/login/LoginActivity.kt
+++ app/src/main/java/com/example/booklibrary/login/LoginActivity.kt
@@ -1,13 +1,127 @@
 package com.example.booklibrary.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.booklibrary.MainActivity
 import com.example.booklibrary.R
+import com.example.booklibrary.databinding.ActivityLoginBinding
+import com.example.booklibrary.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+        setContentView(binding.root)
+//        firebaseAuth = FirebaseAuth.getInstance()
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (username.isEmpty()) {
+                binding.etUsername.error = "Username is required"
+                binding.etUsername.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.etPassword.error = "Password is required"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            loginFirebase(username, password)
+        }
+
+        // Login Google
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        findViewById<Button>(R.id.btn_google).setOnClickListener {
+                signInGoogle()
+        }
+    }
+
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    handleResult(task)
+                }
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>?) {
+        if (task!!.isSuccessful) {
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUi(account)
+            }
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUi(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent : Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loginFirebase(username: String, password: String) {
+        auth.signInWithEmailAndPassword(username, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Welcome $username", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 }
--- app/src/main/java/com/example/booklibrary/register/RegisterActivity.kt
+++ app/src/main/java/com/example/booklibrary/register/RegisterActivity.kt
@@ -1,12 +1,85 @@
 package com.example.booklibrary.register
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.booklibrary.R
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.booklibrary.databinding.ActivityRegisterBinding
+import com.example.booklibrary.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val username = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (email.isEmpty()) {
+                binding.etEmail.error = "Email is required"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.etEmail.error = "Email not valid"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+//            if (username.isEmpty()) {
+//                binding.etEmail.error = "Username is required"
+//                binding.etEmail.requestFocus()
+//                return@setOnClickListener
+//            }
+
+            if (password.isEmpty()) {
+                binding.etPassword.error = "Password is required"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6) {
+                binding.etPassword.error = "Password must be longer than 6 characters"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            registerFirebase(username, password)
+        }
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun registerFirebase(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Register is successfully", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 }
